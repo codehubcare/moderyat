bf036c569d0327895c26ab757265fd2644dd69a5
Shamshad Zaheer <shamshadtech101@gmail.com>
2024-06-28 16:49:28 -0700

    Update service provider file

--- src/ModeryatBaseServiceProvider.php
+++ src/ModeryatBaseServiceProvider.php
@@ -1,14 +1,21 @@
 <?php
 
-namespace codehubcare\Moderyat;
+namespace Codehubcare\Moderyat;
 
-class ModeryatBaseServiceProvider
+use Illuminate\Support\ServiceProvider;
+
+class ModeryatBaseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        // Publishing configuration files
+        $this->publishes([
+            __DIR__ . '../config/settings.php' => config_path('settings.php',)
+        ], 'config');
     }
 
     public function register()
     {
+        // Register bindings, services and etc
     }
 }
