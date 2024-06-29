4ca06f3d9d2bbadba4f653eb32cd208f82d983c8
Shamshad Zaheer <shamshadtech101@gmail.com>
2024-06-28 18:11:28 -0700

    Fix dashboard action bug

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Codehubcare\Moderyat\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function () {
-    Route::get('dashboard');
+    Route::get('dashboard', [DashboardController::class, 'index']);
 });
