c41a8cd54f2f60d3147dd86b5337879ec70fe097
Shamshad Zaheer <shamshadtech101@gmail.com>
2024-07-07 12:26:57 -0700

    finalized vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ["src/resources/sass/app.scss", "src/resources/js/app.js"],
-      refresh: true
-    })
-  ]
+  build: {
+    rollupOptions: {
+      input: ["src/resources/js/app.js", "src/resources/sass/app.scss"],
+      output: {
+        assetFileNames: "[name][extname]",
+        dir: "src/public/moderyat/",
+        entryFileNames: "[name].js"
+      }
+    },
+    manifest: false
+  }
 });
