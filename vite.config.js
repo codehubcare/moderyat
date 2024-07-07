57ae14b873fce01fd6d74513c789faabc2e3eb50
Shamshad Zaheer <shamshadtech101@gmail.com>
2024-07-06 22:06:17 -0700

    configuring vite

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,6 @@
 import { defineConfig } from "vite";
-import { laravel } from "laravel-vite-plugin";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+  plugins: [laravel(["src/resources/css/app.css", "src/resources/js/app.js"])]
+});
